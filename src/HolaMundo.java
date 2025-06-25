bba132e3825b9c09f6355db26c657d5a488d600d
Kenny Mendo <bryan3094@hotmail.com>
2025-06-25 00:36:30 -0500

    Primera Clase Impresion Basico

---
+++ src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    //public es el modificador de acceso
+    //static indica que el método pertenece a la clase, no a una instancia
+    //void indica que el método no devuelve ningún valor
+    //main es el nombre del método al que se llama al iniciar el programa
+    public static void main(String[] args) {
+        System.out.println("Hola Kenny Mendo");
+    }
+}

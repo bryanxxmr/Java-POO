eafa277221b8db36e3806744b57b9ebe88a585d6
Kenny <kennybmendo@innovacxion.com>
2025-06-29 01:12:26 -0500

    Sistema Numericos

--- src/SistemaNumericos.java
+++ src/SistemaNumericos.java
@@ -1,20 +1,32 @@
+import javax.swing.*;
+
 public class SistemaNumericos {
     public static void main(String[] args) {
 
-        int numeroDecimal = 500;
+        String numeroStr=JOptionPane.showInputDialog(null,"Ingrese un numero entero:");
+        int numeroDecimal = Integer.parseInt(numeroStr); //convertir de String a int
         System.out.println("numeroDecimal = " + numeroDecimal);
 
-        System.out.println("numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+        String mensajeBinario = "numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
+        System.out.println(mensajeBinario);
 
         int numeroBinario = 0b111110100; //con el 0b se indica que es un numero binario
         System.out.println("\nnumeroBinario = " + numeroBinario);
 
-        System.out.println("numero octal de = " + numeroBinario + " = " + Integer.toOctalString(numeroDecimal));
+        String mensajeOctal = "numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
+        System.out.println(mensajeOctal);
         int numeroOctal = 0764; //con el 0 antes del numero se indica que es un numero octal
         System.out.println("numeroOctal = " + numeroOctal);
 
-        System.out.println("\nnumero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+        String mensajeHexadecimal = "numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        System.out.println(mensajeHexadecimal);
         int numeroHexadecimal = 0x1f4; //con el 0x se indica que es un numero hexadecimal
         System.out.println("numeroHexadecimal = " + numeroHexadecimal);
+
+        String mensaje = mensajeBinario;
+        mensaje += "\n"+mensajeOctal;
+        mensaje += "\n" + mensajeHexadecimal;
+
+        JOptionPane.showMessageDialog(null,mensaje);
     }
 }
